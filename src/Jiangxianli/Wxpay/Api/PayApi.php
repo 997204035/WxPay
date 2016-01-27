5c2ea64c0cee534da2dad8617cac697f6638ac5e
jiangxianli <997204035@qq.com>
2016-01-27 11:09:45 +0800

    初步提交

---
+++ src/Jiangxianli/Wxpay/Api/PayApi.php
@@ -0,0 +1,279 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 16/1/27
+ * Time: 10:14
+ */
+
+namespace Jiangxianli\Wxpay;
+use Jiangxianli\Wxpay\Data\WxPayDataBase;
+use Jiangxianli\Wxpay\Exception\WxPayException;
+use Jiangxianli\Wxpay\Data\WxPayResults;
+
+
+Trait PayApi {
+
+    public $configs = [];
+
+
+    public function setConfig($configs=[]){
+
+        $this->configs = array_merge(\Config::get('JWxpay::base'),$configs);
+    }
+
+    /**
+     *
+     * 统一下单，WxPayUnifiedOrder中out_trade_no、body、total_fee、trade_type必填
+     * appid、mchid、spbill_create_ip、nonce_str不需要填入
+     * @param WxPayDataBase $payData
+     * @param int $timeOut
+     * @throws WxPayException
+     * @return 成功时返回，其他抛异常
+     */
+    public function unifiedorder(WxPayDataBase $payData,$timeOut=60){
+        $url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+        //检测必填参数
+        if(!$payData->IsOut_trade_noSet()) {
+            throw new WxPayException("缺少统一支付接口必填参数out_trade_no！");
+        }else if(!$payData->IsBodySet()){
+            throw new WxPayException("缺少统一支付接口必填参数body！");
+        }else if(!$payData->IsTotal_feeSet()) {
+            throw new WxPayException("缺少统一支付接口必填参数total_fee！");
+        }else if(!$payData->IsTrade_typeSet()) {
+            throw new WxPayException("缺少统一支付接口必填参数trade_type！");
+        }
+
+        //关联参数
+        if($payData->GetTrade_type() == "JSAPI" && !$payData->IsOpenidSet()){
+            throw new WxPayException("统一支付接口中，缺少必填参数openid！trade_type为JSAPI时，openid为必填参数！");
+        }
+        if($payData->GetTrade_type() == "NATIVE" && !$payData->IsProduct_idSet()){
+            throw new WxPayException("统一支付接口中，缺少必填参数product_id！trade_type为JSAPI时，product_id为必填参数！");
+        }
+
+        //异步通知url未设置，则使用配置文件中的url
+        if(!$payData->IsNotify_urlSet()){
+            $payData->SetNotify_url($this->configs['notify_url']);//异步通知url
+        }
+
+        $payData->SetAppid($this->configs['app_id']);//公众账号ID
+        $payData->SetMch_id($this->configs['mch_id']);//商户号
+        $payData->SetSpbill_create_ip($_SERVER['REMOTE_ADDR']);//终端ip
+        //$inputObj->SetSpbill_create_ip("1.1.1.1");
+        $payData->SetNonce_str($this->getNonceStr());//随机字符串
+
+        //签名
+        $payData->SetSign($this->configs['key']);
+        $xml = $payData->ToXml();
+
+//        $startTimeStamp = $this->getMillisecond();//请求开始时间
+        $response = $this->postXmlCurl($xml, $url, false, $timeOut);
+        $result = WxPayResults::Init($response,$this->configs['key']);
+        //self::reportCostTime($url, $startTimeStamp, $result);//上报请求花费时间
+
+        return $result;
+
+
+    }
+
+    /**
+     * 以post方式提交xml到对应的接口url
+     *
+     * @param string $xml  需要post的xml数据
+     * @param string $url  url
+     * @param bool $useCert 是否需要证书，默认不需要
+     * @param int $second   url执行超时时间，默认30s
+     * @throws WxPayException
+     */
+    public  function postXmlCurl($xml, $url, $useCert = false, $second = 30)
+    {
+        $ch = curl_init();
+        //设置超时
+        curl_setopt($ch, CURLOPT_TIMEOUT, $second);
+
+        //如果有配置代理这里就设置代理
+        if($this->configs['CURL_PROXY_HOST'] != "0.0.0.0"
+            && $this->configs['CURL_PROXY_PORT'] != 0){
+            curl_setopt($ch,CURLOPT_PROXY, $this->configs['CURL_PROXY_HOST']);
+            curl_setopt($ch,CURLOPT_PROXYPORT, $this->configs['CURL_PROXY_PORT']);
+        }
+        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,TRUE);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYHOST,2);//严格校验
+        //设置header
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        //要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+        if($useCert == true){
+            //设置证书
+            //使用证书：cert 与 key 分别属于两个.pem文件
+            curl_setopt($ch,CURLOPT_SSLCERTTYPE,'PEM');
+            curl_setopt($ch,CURLOPT_SSLCERT, $this->configs['SSLCERT_PATH']);
+            curl_setopt($ch,CURLOPT_SSLKEYTYPE,'PEM');
+            curl_setopt($ch,CURLOPT_SSLKEY,$this->configs['SSLKEY_PATH']);
+        }
+        //post提交方式
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
+        //运行curl
+        $data = curl_exec($ch);
+        //返回结果
+        if($data){
+            curl_close($ch);
+            return $data;
+        } else {
+            $error = curl_errno($ch);
+            curl_close($ch);
+            throw new WxPayException("curl出错，错误码:$error");
+        }
+    }
+
+    /**
+     *
+     * 通过跳转获取用户的openid，跳转流程如下：
+     * 1、设置自己需要调回的url及其其他参数，跳转到微信服务器https://open.weixin.qq.com/connect/oauth2/authorize
+     * 2、微信服务处理完成之后会跳转回用户redirect_uri地址，此时会带上一些参数，如：code
+     *
+     * @return 用户的openid
+     */
+    public function GetOpenid()
+    {
+        //通过code获得openid
+        if (!isset($_GET['code'])){
+            //触发微信返回code码
+            $baseUrl = urlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].$_SERVER['QUERY_STRING']);
+            $url = $this->__CreateOauthUrlForCode($baseUrl);
+            Header("Location: $url");
+            exit();
+        } else {
+            //获取code码，以获取openid
+            $code = $_GET['code'];
+            $openid = $this->getOpenidFromMp($code);
+            return $openid;
+        }
+    }
+
+    /**
+     *
+     * 构造获取code的url连接
+     * @param string $redirectUrl 微信服务器回跳的url，需要url编码
+     *
+     * @return 返回构造好的url
+     */
+    private function __CreateOauthUrlForCode($redirectUrl)
+    {
+        $urlObj["appid"] = $this->configs['app_id'];
+        $urlObj["redirect_uri"] = "$redirectUrl";
+        $urlObj["response_type"] = "code";
+        $urlObj["scope"] = "snsapi_base";
+        $urlObj["state"] = "STATE"."#wechat_redirect";
+        $bizString = $this->ToUrlParams($urlObj);
+        return "https://open.weixin.qq.com/connect/oauth2/authorize?".$bizString;
+    }
+
+    /**
+     *
+     * 通过code从工作平台获取openid机器access_token
+     * @param string $code 微信跳转回来带上的code
+     *
+     * @return openid
+     */
+    public function GetOpenidFromMp($code)
+    {
+        $url = $this->__CreateOauthUrlForOpenid($code);
+        //初始化curl
+        $ch = curl_init();
+        //设置超时
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->curl_timeout);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,FALSE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        if($this->configs['CURL_PROXY_HOST'] != "0.0.0.0"
+            && $this->configs['CURL_PROXY_PORT'] != 0){
+            curl_setopt($ch,CURLOPT_PROXY, $this->configs['CURL_PROXY_HOST']);
+            curl_setopt($ch,CURLOPT_PROXYPORT, $this->configs['CURL_PROXY_PORT']);
+        }
+        //运行curl，结果以jason形式返回
+        $res = curl_exec($ch);
+        curl_close($ch);
+        //取出openid
+        $data = json_decode($res,true);
+        $this->data = $data;
+        $openid = $data['openid'];
+        return $openid;
+    }
+
+    /**
+     *
+     * 构造获取open和access_toke的url地址
+     * @param string $code，微信跳转带回的code
+     *
+     * @return 请求的url
+     */
+    private function __CreateOauthUrlForOpenid($code)
+    {
+        $urlObj["appid"] = $this->configs['app_id'];
+        $urlObj["secret"] = $this->configs['app_secret'];
+        $urlObj["code"] = $code;
+        $urlObj["grant_type"] = "authorization_code";
+        $bizString = $this->ToUrlParams($urlObj);
+        return "https://api.weixin.qq.com/sns/oauth2/access_token?".$bizString;
+    }
+
+    /**
+     *
+     * 拼接签名字符串
+     * @param array $urlObj
+     *
+     * @return 返回已经拼接好的字符串
+     */
+    private function ToUrlParams($urlObj)
+    {
+        $buff = "";
+        foreach ($urlObj as $k => $v)
+        {
+            if($k != "sign"){
+                $buff .= $k . "=" . $v . "&";
+            }
+        }
+
+        $buff = trim($buff, "&");
+        return $buff;
+    }
+
+    /**
+     *
+     * 产生随机字符串，不长于32位
+     * @param int $length
+     * @return 产生的随机字符串
+     */
+    public  function getNonceStr($length = 32)
+    {
+        $chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+        $str ="";
+        for ( $i = 0; $i < $length; $i++ )  {
+            $str .= substr($chars, mt_rand(0, strlen($chars)-1), 1);
+        }
+        return $str;
+    }
+
+    /**
+     * 获取毫秒级别的时间戳
+     */
+    public   function getMillisecond()
+    {
+        //获取毫秒的时间戳
+        $time = explode ( " ", microtime () );
+        $time = $time[1] . ($time[0] * 1000);
+        $time2 = explode( ".", $time );
+        $time = $time2[0];
+        return $time;
+    }
+
+
+
+} 
---
+++ src/Jiangxianli/Wxpay/Pay/JsApiPay.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Jiangxianli\Wxpay\Pay;
+use Jiangxianli\Wxpay\PayApi;
+use Jiangxianli\Wxpay\Data\WxPayUnifiedOrder;
+use Jiangxianli\Wxpay\Exception\WxPayException;
+use Jiangxianli\Wxpay\Data\WxPayJsApiPay;
+use Illuminate\View\View;
+
+class JsApiPay {
+
+    use PayApi;
+
+    /**
+     * 支付请求并返回支付页面
+     * @return mixed
+     * @throws WxPayException
+     */
+    public function pay($configs = []){
+
+        $this->setConfig($configs);
+
+        //①、获取用户openid
+        $openId = $this->GetOpenid();
+
+        //②、统一下单
+        $input  = new WxPayUnifiedOrder();
+        $input->SetBody($this->configs['body']);
+        $input->SetAttach($this->configs['attach']);
+        $input->SetOut_trade_no($this->configs['mch_id'] . date("YmdHis"));
+        $input->SetTotal_fee($this->configs['total_fee']);
+        $input->SetTime_start(date("YmdHis"));
+        $input->SetTime_expire(date("YmdHis", time() + 600));
+        $input->SetGoods_tag($this->configs['goods_tag']);
+        $input->SetNotify_url($this->configs['notify_url']);
+        $input->SetTrade_type("JSAPI");
+        $input->SetOpenid($openId);
+        $order = $this->unifiedOrder($input);
+//        echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
+//        printf_info($order);
+        $jsApiParameters = $this->GetJsApiParameters($order);
+
+//        //获取共享收货地址js函数参数
+//        $editAddress = $this->GetEditAddressParameters();
+
+        //③、在支持成功回调通知中处理成功之后的事宜，见 notify.php
+        /**
+         * 注意：
+         * 1、当你的回调地址不可访问的时候，回调通知会失败，可以通过查询订单来确认支付是否成功
+         * 2、jsapi支付时需要填入用户openid，WxPay.JsApiPay.php中有获取openid流程 （文档可以参考微信公众平台“网页授权接口”，
+         * 参考http://mp.weixin.qq.com/wiki/17/c0f37d5704f0b64713d5d2c37b468d75.html）
+         */
+
+        return View::make('JWxPay::jsApiPay',compact('jsApiParameters'))->render();
+
+    }
+
+
+    /**
+     *
+     * 获取jsapi支付的参数
+     * @param array $UnifiedOrderResult 统一支付接口返回的数据
+     * @throws WxPayException
+     *
+     * @return json数据，可直接填入js函数作为参数
+     */
+    public function GetJsApiParameters($UnifiedOrderResult)
+    {
+        if(!array_key_exists("appid", $UnifiedOrderResult)
+            || !array_key_exists("prepay_id", $UnifiedOrderResult)
+            || $UnifiedOrderResult['prepay_id'] == "")
+        {
+            throw new WxPayException("参数错误");
+        }
+        $jsapi = new WxPayJsApiPay();
+        $jsapi->SetAppid($UnifiedOrderResult["appid"]);
+        $timeStamp = time();
+        $jsapi->SetTimeStamp("$timeStamp");
+        $jsapi->SetNonceStr($this->getNonceStr());
+        $jsapi->SetPackage("prepay_id=" . $UnifiedOrderResult['prepay_id']);
+        $jsapi->SetSignType("MD5");
+        $jsapi->SetPaySign($jsapi->MakeSign($this->configs['key']));
+        $parameters = json_encode($jsapi->GetValues());
+        return $parameters;
+    }
+
+} 
